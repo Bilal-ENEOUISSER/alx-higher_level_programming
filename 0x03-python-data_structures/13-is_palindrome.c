fb4e195a59ccbcc27e9797c91c5e96b667f34e92
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-01-07 17:14:42 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,51 +3,57 @@
 #include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
+ * reverse_listint - Reverses a linked list in place
+ * @head: Pointer to the head of the list
+ * Return: Pointer to the new head of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *new;
+    listint_t *prev = NULL, *current = *head, *next = NULL;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+    return *head;
 }
+
 /**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
+ * is_palindrome - Identifies if a singly linked list is a palindrome
+ * @head: Pointer to the head of the list
+ * Return: 1 if it is palindrome else 0
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
+    listint_t *slow = *head, *fast = *head, *prev_slow = *head;
+    
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
 
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
-	free_listint(aux);
-	return (1);
+    // Move fast to the end and slow to the middle
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        prev_slow = slow;
+        slow = slow->next;
+    }
+
+    // Reverse the second half of the list
+    prev_slow->next = reverse_listint(&slow);
+
+    // Compare the reversed second half with the first half
+    while (slow != NULL)
+    {
+        if ((*head)->n != slow->n)
+            return 0;
+        *head = (*head)->next;
+        slow = slow->next;
+    }
+
+    return 1;
 }
